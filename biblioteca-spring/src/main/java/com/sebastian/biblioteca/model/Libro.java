811e02ec8fb77605ef5310c2893bb5f0129637d4
Sebastian Hood <se.hood@duocuc.cl>
2026-03-19 13:34:29 -0300

    add libro model class

---
+++ biblioteca-spring/src/main/java/com/sebastian/biblioteca/model/Libro.java
@@ -0,0 +1,17 @@
+package com.sebastian.biblioteca.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Libro {
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+}
