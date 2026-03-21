627ae384d0774113bd3d2ce17f7883188fea12a3
Sebastian Hood <se.hood@duocuc.cl>
2026-03-21 12:23:03 -0300

    add validation to libro model

--- biblioteca-spring/src/main/java/com/sebastian/biblioteca/model/Libro.java
+++ biblioteca-spring/src/main/java/com/sebastian/biblioteca/model/Libro.java
@@ -1,18 +1,32 @@
 package com.sebastian.biblioteca.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+@NotBlank
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Libro {
+    
     private int id;
+
+    @NotBlank(message = "El ISBN es obligatorio")
     private String isbn;
+
+    @NotBlank(message = "El titulo es obligatorio")
     private String titulo;
+
     private String editorial;
+
+    @Min(value = 1900, message = "Fecha inválida")
     private int fechaPublicacion;
+
+    @NotBlank(message = "El autor es obligatorio")
     private String autor;
 }
